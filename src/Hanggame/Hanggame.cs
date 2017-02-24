c00be68118765c0a72682a2869b650850d329a81
mitutee <ex@ex.ex>
2017-02-24 07:51:05 +0200

    Firs working version.

--- src/Hanggame/Hanggame.cs
+++ src/Hanggame/Hanggame.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Hanggame
@@ -9,28 +11,130 @@ namespace Hanggame
     {
         public CommunicationChannel Chat = new CommunicationChannel();
 
+        public Encoding enc = Encoding.GetEncoding(65001);
+
         public void PlayGame()
         {
+            loadWords();
             //Вывод правил игры
-            Chat.Write("Приветствую тебя возле 'Виселицы'!"
+            Chat.WriteLine("Приветствую тебя возле 'Виселицы'!"
                 + "Правила игры следующие:\n"
                 + " 1. Я загадываю слово;"
                 + " 2. "
                 );
             
-            Chat.Write("Я загадал слово!");
+            Chat.WriteLine("Я загадал слово!");
 
             bool doYouWantToPlay = true;
             while (doYouWantToPlay) {
                 // Setting up the game
-                Hangsession game = new Hangsession();
-            while (!game.GameOver()) {
-                    // Playing the game
+                Hangsession game = new Hangsession(words);
 
+                do {
+                    Chat.WriteLine(game.DrawPicture());
+                    Chat.WriteLine(game.getFormalCurrentGuess());
+                    Chat.WriteLine(game.mysteryWord);
+                    Chat.WriteLine("Попытайся догадаться, какая буква есть в этом слове..");
+                    // Get the guess
+                    get_the_guess:
+                    string input = Chat.Read().ToUpper();
+                    //-- Is input valid ? --
+                        if(input.Length > 1) {
+                        Chat.WriteLine("Угадывать нужно по одной букве! Попробуй еще раз!");
+                        goto get_the_guess;
+                    }
+                    char guess = (char)input[0];
+
+                    // Was it guessed before ?
+                    if (game.IsGuessedAlready(guess)) {
+                        Chat.WriteLine("Эта буква уже была! Попробуй еще раз!");
+                        goto get_the_guess;
+                    }
+                    // Playing the game
+                    if (game.PlayGuess(guess)) {
+
+                        if (game.didWeWin()) {
+                            Chat.WriteLine("Урра! ты победил!");
+                            goto ask_for_next_game;
+                        }
+
+                        Chat.WriteLine("Действительно.. Эта буква есть в слове, ты угадал!");
+                    }
+                    else {
+                        if (game.didWeLose()) {
+                            Chat.WriteLine($"Попытки закончились и ты проиграл! Загаданное слово: \n{game.mysteryWord}");
+                            goto ask_for_next_game;
+                        }
+                        Chat.WriteLine("Не-а.. Такой буквы в слове нет!");
+                    }
+
+
+                }
+                while (!game.GameOver());
+                ask_for_next_game:
+
+                Chat.WriteLine("++++++++++++++++++++++++++++++++++++++++++++");
+                Chat.WriteLine("Желаешь сыграть еще?");
+                string answer = Chat.Read().ToLower();
+
+                if (positiveAnswers.Contains(answer)) {
+                    Chat.WriteLine("Отлично! Начинаем по новой!");
+                    doYouWantToPlay = true;
+                }
+                else if (negativeAnswers.Contains(answer)) {
+                    Chat.WriteLine("Ну что ж! Увидимся в следующий раз!");
+                    doYouWantToPlay = false;
+                }
+                else {
+                    Chat.WriteLine("Я не расслышал твоего ответа.. Будь добр, повтори.");
+                    goto ask_for_next_game;
                 }
             }
 
             
+        }
+
+        private List<string> positiveAnswers = new List<string>() {
+            "y",
+            "yes",
+            "da",
+            "d",
+            "д",
+            "да",
+            "давай",
+
+        };
+        private List<string> negativeAnswers = new List<string>() {
+            "n",
+            "no",
+            "net",
+            "ne",
+            "н",
+            "нет",
+            "не",
+
+        };
+
+        private List<string> words = new List<string>();
+        /// <summary>
+        /// Fulfil <see cref="words"/> with words from file
+        /// </summary>
+        private List<string> loadWords() {
+            try {
+                using (var sr = new StreamReader(File.OpenRead(Directory.GetCurrentDirectory().ToString() +
+            "\\Words.txt"))) {
+                    string word = sr.ReadLine();
+                    while (word != null) {
+                        words.Add(word);
+                        word = sr.ReadLine();
+                    }
+                }
+            }
+            catch (Exception e) {
+
+                Console.WriteLine(e.Message);
+            }
+            return words;
         }
 
     }
@@ -38,12 +142,13 @@ namespace Hanggame
 
     public class CommunicationChannel
     {
-        public void Write(string s)
+        public void WriteLine(string s = "\n")
         {
-            Console.WriteLine(s);
+            Console.WriteLine(s + "\n");
         }
         public string Read()
         {
+            Console.InputEncoding = UTF8Encoding.Unicode;
             return Console.ReadLine();
         }
     }
--- src/Hanggame/Hangsession.cs
+++ src/Hanggame/Hangsession.cs
@@ -9,26 +9,91 @@ namespace Hanggame
 {
     public class Hangsession {
 
-        private String mysteryWord;
-        private StringBuilder currentGuess;
-        private List<char> previousGuesses = new List<char>();
+        public String mysteryWord; // Word to be guessed
+        private StringBuilder currentGuess;// Current state
+        private List<char> previousGuesses = new List<char>(); // Letters that were chosen
 
         private const int MAX_TRIES = 6;
         private int currentTry = 0;
 
-        private List<string> words = new List<string>();
+        private List<string> words = new List<string>();// List of words to random from
 
 
-        public Hangsession() {
-            loadWords();
-            for (int i = 0; i < 6; i++) {
-                Console.WriteLine( DrawPicture() ); 
-                currentTry++;
-            }
-            mysteryWord = pickWord();
+        public Hangsession(List<string> word_bank) {
+
+            words = word_bank;
+            mysteryWord = pickWord().ToUpper();
             currentGuess = initializeCurrentGuess();
         }
 
+        //_ _ F _ _ G
+        public string getFormalCurrentGuess() => "Слово: " + currentGuess.ToString();
+
+        private StringBuilder initializeCurrentGuess() {
+            StringBuilder initial = new StringBuilder();
+            for(int i = 0; i < mysteryWord.Length * 2; i++) {
+                char c = i % 2 == 0 ? '_' : ' ';
+                initial.Append(c);
+            }
+            return initial;
+        }
+
+        private string pickWord() => words[(new Random()).Next(0, words.Count - 1)];
+
+        internal bool IsGuessedAlready(char c) {
+            return previousGuesses.Contains(c);
+        }
+
+        internal bool GameOver() {
+            if (didWeWin()) {
+                return true;
+            }
+            else if (didWeLose()) {
+                return true;
+            }
+            return false;
+        }
+        #region Helpers for GameOver()
+
+        public bool didWeLose() {
+            return currentTry >= MAX_TRIES;
+        }
+
+
+
+        public bool didWeWin() {
+            string unformal_guess = getCondensendCurrentGuess();
+            return unformal_guess.Equals(mysteryWord);
+        }
+        public string getCondensendCurrentGuess() {
+            string guess = currentGuess.ToString();
+            return guess.Replace(" ", "");
+        }
+        #endregion
+
+        internal bool PlayGuess(char guess) {
+            bool isItAGoodGuess = false;
+            previousGuesses.Add(guess);
+            for (int i = 0; i < mysteryWord.Length; i++) {
+                if(mysteryWord[i] == guess) {
+                    isItAGoodGuess = true;
+                    currentGuess.Replace('_', guess, i * 2, 1);
+                    //currentGuess.Insert(i * 2 , guess);
+
+                }
+            }
+            if (!isItAGoodGuess) {
+                currentTry++;
+            }
+            return isItAGoodGuess;
+
+        }
+
+        #region Drawing
+        /// <summary>
+        /// Draws the picture according to current guess
+        /// </summary>
+        /// <returns>String that represents gallows</returns>
         public string DrawPicture() {
             switch (currentTry) {
                 case 0: return noPersonDraw();
@@ -36,12 +101,15 @@ namespace Hanggame
                 case 2: return addBodyDraw();
                 case 3: return addOneArmDraw();
                 case 4: return addSecondArmDraw();
+                case 5: return addFirstLegDraw();
                 default: return fullPersonDraw();
 
             }
         }
 
-        private string fullPersonDraw() {
+
+
+        private string addFirstLegDraw() {
             return ("   _____\n")
              + ("  |     |\n")
              + ("  |     O\n")
@@ -51,12 +119,22 @@ namespace Hanggame
              + ("  |\n")
              + ("__|__\n");
         }
+        private string fullPersonDraw() {
+            return ("   _____\n")
+                 + ("  |     |\n")
+                 + ("  |     O\n")
+                 + ("  |    \\|/\n")
+                 + ("  |     |\n")
+                 + ("  |    / \n")
+                 + ("  |\n")
+                 + ("__|__\n");
+        }
 
         private string addSecondArmDraw() {
             return ("   _____\n")
              + ("  |     |\n")
              + ("  |     O\n")
-             + ("  |    \\|/n")
+             + ("  |    \\|/\n")
              + ("  |      |\n")
              + ("  |\n")
              + ("  |\n")
@@ -97,44 +175,17 @@ namespace Hanggame
         }
 
         private string noPersonDraw() {
-           return ("   _____\n")
-            +("  |     |\n")
-            +("  |     \n")
-            +("  |\n")
-            +("  |\n")
-            +("  |\n")
-            +("  |\n")
-            + ("__|__\n");
+            return ("   _____\n")
+             + ("  |     |\n")
+             + ("  |     \n")
+             + ("  |\n")
+             + ("  |\n")
+             + ("  |\n")
+             + ("  |\n")
+             + ("__|__\n");
         }
 
-        private StringBuilder initializeCurrentGuess() {
-            StringBuilder initial = new StringBuilder();
-            for(int i = 0; i < mysteryWord.Length * 2; i++) {
-                char c = i % 2 == 0 ? '_' : ' ';
-                initial.Append(c);
-            }
-            return initial;
-        }
-
-        private string pickWord() => words[(new Random(456)).Next(0, words.Count - 1)];
-
-        /// <summary>
-        /// Fulfil <see cref="words"/> with words from file
-        /// </summary>
-        private void loadWords() {
-            using (var sr = new StreamReader(File.OpenRead(Directory.GetCurrentDirectory().ToString() + 
-                "\\Words.txt"))) {
-                string word = sr.ReadLine();
-                while (word != null) {
-                    words.Add(word);
-                    word = sr.ReadLine();
-                }
-            }
-        }
-
-        internal bool GameOver() {
-            return false;
-        }
+        #endregion
     }
 
 }
